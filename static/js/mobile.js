dbca5933058742f038ef72d673ba3d9bf75b5b2f
Александр Камышанов <Sanyaelektron@gmail.com>
2025-08-13 12:26:37 +0200

    Добавил главную страницу и статьи

--- static/js/mobile.js
+++ static/js/mobile.js
@@ -47,6 +47,25 @@ window.onload = function () {
         .catch(error => {
             console.error('Ошибка:', error);
         });
+    
+    const articles_div = document.querySelector(".articles");
+    if (articles_div !== null) {
+        fetch('/articles.json')
+            .then(response => response.json())
+            .then(data => {
+                for (const article of data.slice(-5)) {
+                    const article_div = document.createElement("div");
+                    article_div.setAttribute("class", "heading");
+                    article_div.setAttribute("onclick", "redirect('" + article.href + "mobile/')")
+                    article_div.setAttribute("style", "background-image: url(" + article.image + "); cursor: pointer;")
+                    article_div.innerHTML = "<div class=\"header-content\"><span class=\"heading-h\">" + article.heading + "</span><br><span class=\"heading-m\">" + article.theme + "</span></div>";
+                    articles_div.appendChild(article_div);
+                }
+            })
+            .catch(error => {
+                console.error('Ошибка:', error);
+            });
+    }
 }
 
 if (!IsMobile()) {
